5d411484fb65d5e4413bd661f3056a6682cf5c18
cmh19950807 <dill89587@gmail.com>
2023-08-12 14:27:35 +0900

    mac setting

--- main.dart
+++ main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/screens/home_screen.dart';
+import 'package:fp1/screens/home_screen.dart';
+//import 'package:flutter_application_1/screens/home_screen.dart';
 
-//
 void main() {
   runApp(const App());
 }
