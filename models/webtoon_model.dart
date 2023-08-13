4a2c722117602a39bc3ff93c0cffa35ec8281c87
cmh19950807 <dill89587@gmail.com>
2023-08-13 18:08:08 +0900

    save 6

--- models/webtoon_model.dart
+++ models/webtoon_model.dart
@@ -1,4 +1,7 @@
 class WebtoonModel {
   final String title, thumb, id;
-  WebtoonModel({required this.id, required this.title, required this.thumb});
+  WebtoonModel.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        thumb = json['thumb'],
+        id = json['id'];
 }
--- services/api_services.dart
+++ services/api_services.dart
@@ -1,15 +1,22 @@
+import 'dart:convert';
+
+import 'package:fp1/models/webtoon_model.dart';
 import 'package:http/http.dart' as http;
 
 class Apiservice {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToon() async {
+  Future<List<WebtoonModel>> getTodaysToon() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
     }
     throw Error();
   }
