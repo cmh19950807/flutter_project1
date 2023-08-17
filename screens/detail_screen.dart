2b6402e3804cdccf2ae0347502ee5c9e3ed74a6d
cmh19950807 <dill89587@gmail.com>
2023-08-17 23:59:56 +0900

    save 6.13

--- screens/detail_screen.dart
+++ screens/detail_screen.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:fp1/models/webtoon_detail_model.dart';
+import 'package:fp1/models/webtoon_episode_model.dart';
+import 'package:fp1/services/api_services.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
   const DetailScreen(
       {super.key, required this.title, required this.id, required this.thumb});
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisdoeModel>> episodes;
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +31,7 @@ class DetailScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
         ),
       ),
@@ -27,7 +44,7 @@ class DetailScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Hero(
-                tag: id,
+                tag: widget.id,
                 child: Container(
                   width: 250,
                   clipBehavior: Clip.hardEdge,
@@ -40,7 +57,7 @@ class DetailScreen extends StatelessWidget {
                             color: Colors.black.withOpacity(0.5))
                       ]),
                   child: Image.network(
-                    thumb,
+                    widget.thumb,
                     headers: const {
                       "User-Agent":
                           "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
