100a8adaa76eaac35cc66811a6159c6f270957cf
cmh19950807 <dill89587@gmail.com>
2023-08-25 18:14:26 +0900

    new branch set

--- main.dart
+++ main.dart
@@ -1,33 +1,5 @@
 import 'package:flutter/material.dart';
 
-//win
-//import 'package:flutter_application_1/screens/home_screens.dart';
-
-//mac
-import 'package:flutter_application_1/screens/home_screens.dart';
-
-//import 'package:flutter_application_1/screens/home_screen.dart';
-
-// install list
-// flutter pub add url_launcher
-// flutter pub add http
-// flutter pub add shared_preferences
-
-//url_launcher 경로에 붙여 넣기 필요
-// https://pub.dev/packages/url_launcher
-
-//win    <!-- If your app checks for SMS support -->
-// <intent>
-//     <action android:name="android.intent.action.VIEW" />
-//     <data android:scheme="https" />
-// </intent>
-
-//ios
-// <key>LSApplicationQueriesSchemes</key>
-// <array>
-//   <string>https</string>
-// </array>
-
 void main() {
   runApp(const App());
 }
@@ -37,8 +9,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
-    );
+    return const MaterialApp();
   }
 }
