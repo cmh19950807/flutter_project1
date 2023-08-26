22b74780b5ccc4c4d8358f62b01cf4c75a3e3e39
cmh19950807 <dill89587@gmail.com>
2023-08-26 14:59:06 +0900

    save file

--- main.dart
+++ main.dart
@@ -1,14 +1,142 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const App());
-}
+void main() => runApp(const MyApp());
 
-class App extends StatelessWidget {
-  const App({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      title: 'Charactor',
+      home: Grade(),
+    );
+  }
+}
+
+class Grade extends StatelessWidget {
+  const Grade({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber[800],
+      appBar: AppBar(
+        title: const Text('BBANTO'),
+        titleTextStyle:
+            const TextStyle(fontSize: 28, fontWeight: FontWeight.w500),
+        backgroundColor: Colors.amber[700],
+        elevation: 0.0,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'NAME',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0.8),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'BBANTO',
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  letterSpacing: 1.2,
+                  fontWeight: FontWeight.w800),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Text(
+                  'BBANTO POWER LEVEL',
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                      letterSpacing: 0.7,
+                      fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  '14',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      letterSpacing: 0.7,
+                      fontWeight: FontWeight.w800),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Using light',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Using light',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Using light',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
