870516ac4258e99d53652466d75e2494ce9d3119
cmh19950807 <dill89587@gmail.com>
2023-08-26 16:08:42 +0900

    svae

--- main.dart
+++ main.dart
@@ -14,157 +14,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Charactor',
-      home: Grade(),
+      title: 'Appbar',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const MyPage(),
     );
   }
 }
 
-class Grade extends StatelessWidget {
-  const Grade({super.key});
+class MyPage extends StatelessWidget {
+  const MyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: const Text('BBANTO'),
-        titleTextStyle:
-            const TextStyle(fontSize: 28, fontWeight: FontWeight.w500),
-        backgroundColor: Colors.amber[700],
-        elevation: 0.0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30, 40, 30, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('image/1.jpeg'),
-                radius: 50,
-              ),
-            ),
-            Divider(
-              height: 55,
-              color: Colors.amber[100],
-              thickness: 1,
-            ),
-            const Text(
-              'NAME',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                  letterSpacing: 0.8),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              'BBANTO',
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-                  letterSpacing: 1.2,
-                  fontWeight: FontWeight.w800),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Row(
-              children: [
-                Text(
-                  'BBANTO POWER LEVEL',
-                  style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                      letterSpacing: 0.7,
-                      fontWeight: FontWeight.w600),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                Text(
-                  '14',
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                      letterSpacing: 0.7,
-                      fontWeight: FontWeight.w800),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Row(
-              children: [
-                Icon(
-                  Icons.check_circle_outline,
-                  color: Colors.white,
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  'Using light',
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.w500),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Row(
-              children: [
-                Icon(
-                  Icons.check_circle_outline,
-                  color: Colors.white,
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  'Using light',
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.w500),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Row(
-              children: [
-                Icon(
-                  Icons.check_circle_outline,
-                  color: Colors.white,
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  'Using light',
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.w500),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('image/2.jpeg'),
-                backgroundColor: Colors.amber,
-                radius: 70,
-              ),
-            ),
-          ],
-        ),
+        title: const Text('Appbar iconn menu'),
+        elevation: 0,
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+        ],
       ),
     );
   }
