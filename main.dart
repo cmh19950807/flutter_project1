9f4eb7b8445f6bb6ebdfc0a8951b75d21d2399af
cmh19950807 <dill89587@gmail.com>
2023-08-27 12:28:41 +0900

    -

--- main.dart
+++ main.dart
@@ -30,96 +30,24 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Appbar iconn menu'),
-        elevation: 0,
-        //leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.shopping_cart),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.search),
-          ),
-        ],
+        title: const Text('Snack Bar'),
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: const CircleAvatar(
-                backgroundImage: AssetImage('image/1.jpeg'),
+      body: Center(
+        child: TextButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.red),
+          ),
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Hi'),
               ),
-              accountName: const Text(
-                'CMH',
-                style: TextStyle(fontWeight: FontWeight.w600),
-              ),
-              accountEmail: const Text(
-                'cmh@naver.com',
-                style: TextStyle(fontWeight: FontWeight.w600),
-              ),
-              otherAccountsPictures: const [
-                CircleAvatar(
-                  backgroundImage: AssetImage('image/2.jpeg'),
-                )
-              ],
-              onDetailsPressed: () {},
-              decoration: BoxDecoration(
-                  color: Colors.red[300],
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40),
-                  )),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.home,
-                color: Colors.grey[850],
-              ),
-              title: const Text('HOME'),
-              onTap: () {
-                TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text('data'));
-              },
-              trailing: const Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.home,
-                color: Colors.grey[850],
-              ),
-              title: const Text('HOME'),
-              onTap: () {
-                TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text('data'));
-              },
-              trailing: const Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.home,
-                color: Colors.grey[850],
-              ),
-              title: const Text('HOME'),
-              onTap: () {
-                TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text('data'));
-              },
-              trailing: const Icon(Icons.add),
-            )
-          ],
+            );
+          },
+          child: const Text(
+            'SHOW',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
       ),
     );
