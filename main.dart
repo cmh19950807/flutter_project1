6813f29fd22761461a65d8e554f7c35750dc9040
cmh19950807 <dill89587@gmail.com>
2023-10-02 15:17:11 +0900

    save mid

--- main.dart
+++ main.dart
@@ -24,16 +24,118 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          textTheme: const TextTheme(
-              // ignore: deprecated_member_use
-              bodyText2: TextStyle(
-            fontSize: 30,
-            color: Colors.white,
-          ))),
-      home: Container(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dice Game',
+      home: LogIn(),
+    );
+  }
+}
+
+class LogIn extends StatefulWidget {
+  const LogIn({super.key});
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  TextEditingController controller = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Log In'),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          )
+        ],
+      ),
+      body: Builder(
+        builder: (context) {
+          return SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.only(top: 50),
+                ),
+                const Center(
+                  child: Image(
+                    image: AssetImage('image/2.jpeg'),
+                    width: 170,
+                    height: 190,
+                  ),
+                ),
+                Form(
+                  child: Theme(
+                      data: ThemeData(
+                        primaryColor: Colors.teal,
+                        inputDecorationTheme: const InputDecorationTheme(
+                          labelStyle: TextStyle(
+                            color: Colors.teal,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      child: Container(
+                        padding: const EdgeInsets.all(40),
+                        child: Column(
+                          children: <Widget>[
+                            TextField(
+                              controller: controller,
+                              decoration: const InputDecoration(
+                                  labelText: 'Enter dice'),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                            TextField(
+                              controller: controller2,
+                              decoration: const InputDecoration(
+                                  labelText: 'Enter Password'),
+                              keyboardType: TextInputType.text,
+                              obscureText: true,
+                            ),
+                            const SizedBox(
+                              height: 40,
+                            ),
+                            ButtonTheme(
+                              minWidth: 100,
+                              height: 50,
+                              buttonColor: Colors.orangeAccent,
+                              child: ElevatedButton(
+                                child: const Icon(
+                                  Icons.arrow_forward,
+                                  color: Colors.white,
+                                  size: 35,
+                                ),
+                                onPressed: () {
+                                  if (controller.text == 'dice' &&
+                                      controller2.text == '1234') {
+                                  } else if (controller.text != 'dice' &&
+                                      controller2.text == '1234') {
+                                  } else if (controller.text == 'dice' &&
+                                      controller2.text != '1234') {
+                                  } else {}
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      )),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
